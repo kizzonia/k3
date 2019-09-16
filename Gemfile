83637eefe991afce3910812d7ce017114cac7302
Kizzonia <chibuikefortune@hotmail.com>
2019-09-16 21:16:22 +0100

    first push  admin and products

---
+++ Gemfile
@@ -0,0 +1,73 @@
+source 'https://rubygems.org'
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+
+ruby '2.5.3'
+
+gem 'rails', '~> 5.2.2'
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 3.11'
+gem 'carrierwave'
+gem "mini_magick"
+gem "fog-aws"
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+gem 'turbolinks', '~> 5'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.5'
+gem 'therubyracer', platforms: :ruby
+gem 'bootstrap', '~> 4.3.1'
+gem 'devise', github: 'plataformatec/devise'
+gem 'ckeditor', '4.2.4'
+gem "just-datetime-picker"
+gem 'httparty', '~> 0.13.7'
+gem "simple_form"
+gem 'friendly_id'
+gem 'solidus'
+gem 'solidus_auth_devise'
+gem 'jquery-rails'
+gem 'sitemap_generator'
+gem 'social-share-button', '~> 0.1.6'
+gem 'activeadmin', github: 'activeadmin'
+gem "mail_form", ">= 1.3.0"
+gem 'toastr-rails'
+gem 'coffee-script-source', '1.8.0'
+
+# Use Redis adapter to run Action Cable in production
+# gem 'redis', '~> 4.0'
+# Use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# Use ActiveStorage variant
+# gem 'mini_magick', '~> 4.8'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
+
+# Reduces boot times through caching; required in config/boot.rb
+gem 'bootsnap', '>= 1.1.0', require: false
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
+
+group :development do
+  # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'web-console', '>= 3.3.0'
+end
+
+group :test do
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '>= 2.15'
+  gem 'selenium-webdriver'
+  # Easy installation and use of chromedriver to run system tests with Chrome
+  gem 'chromedriver-helper'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/initializers/spree.rb
@@ -0,0 +1,118 @@
+# Configure Solidus Preferences
+# See http://docs.solidus.io/Spree/AppConfiguration.html for details
+
+Spree.config do |config|
+  # Core:
+
+  # Default currency for new sites
+  config.currency = "USD"
+
+  # from address for transactional emails
+  config.mails_from = "store@example.com"
+  if Rails.env.production?
+    attachment_config = {
+      s3_credentials: {
+        access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        bucket:            ENV['S3_BUCKET_NAME']
+      },
+
+      storage:        :s3,
+      s3_headers:     { 'Cache-Control' => 'max-age=31557600' },
+      s3_protocol:    'https',
+      bucket:         ENV['S3_BUCKET_NAME'],
+      url:            ':s3_domain_url',
+
+      styles: {
+        mini:     '48x48>',
+        small:    '100x100>',
+        product:  '240x240>',
+        large:    '600x600>'
+      },
+
+      path:           '/:class/:id/:style/:basename.:extension',
+      default_url:    'noimage/:style.png',
+      default_style:  'product'
+    }
+
+    attachment_config.each do |key, value|
+      Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+    end
+  end
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
+
+  # When set, product caches are only invalidated when they fall below or rise
+  # above the inventory_cache_threshold that is set. Default is to invalidate cache on
+  # any inventory changes.
+  # config.inventory_cache_threshold = 3
+
+  # Enable Paperclip adapter for attachments on images and taxons
+  config.image_attachment_module = 'Spree::Image::PaperclipAttachment'
+  config.taxon_attachment_module = 'Spree::Taxon::PaperclipAttachment'
+
+
+  # Permission Sets:
+
+  # Uncomment and customize the following line to add custom permission sets
+  # to a custom users role:
+  # config.roles.assign_permissions :role_name, ['Spree::PermissionSets::CustomPermissionSet']
+
+
+  # Frontend:
+
+  # Custom logo for the frontend
+   config.logo = "logo.png"
+
+  # Template to use when rendering layout
+  # config.layout = "spree/layouts/spree_application"
+
+
+  # Admin:
+
+  # Custom logo for the admin
+  config.admin_interface_logo = "logo.png"
+
+  # Gateway credentials can be configured statically here and referenced from
+  # the admin. They can also be fully configured from the admin.
+  #
+  # Please note that you need to use the solidus_stripe gem to have
+  # Stripe working: https://github.com/solidusio-contrib/solidus_stripe
+  #
+  # config.static_model_preferences.add(
+  #   Spree::PaymentMethod::StripeCreditCard,
+  #   'stripe_env_credentials',
+  #   secret_key: ENV['STRIPE_SECRET_KEY'],
+  #   publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  #   server: Rails.env.production? ? 'production' : 'test',
+  #   test_mode: !Rails.env.production?
+  # )
+end
+
+Spree::Frontend::Config.configure do |config|
+  config.locale = 'en'
+end
+
+Spree::Backend::Config.configure do |config|
+  config.locale = 'en'
+
+  # Uncomment and change the following configuration if you want to add
+  # a new menu item:
+  #
+  # config.menu_items << config.class::MenuItem.new(
+  #   [:section],
+  #   'icon-name',
+  #   url: 'https://solidus.io/'
+  # )
+end
+
+Spree::Api::Config.configure do |config|
+  config.requires_authentication = true
+end
+
+Spree.user_class = "Spree::LegacyUser"
+
+# If you want to add a field to the whitelisted ransackable attributes,
+# just uncomment the following code and change it as you need.
+#
+# Spree::Model.whitelisted_ransackable_attributes << 'field'
